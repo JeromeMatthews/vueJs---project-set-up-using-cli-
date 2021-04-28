a9dc2b2d2ea0e2670a91055c9aa4c83c5fa8cce8
Jerome Matthews <jerome_matthews@live.com>
2021-04-28 15:45:22 -0500

    Changed files: App.vue + index.html
    Created a component and attempted to register it in the main app.vue file with an export.

---
+++ vue-framework-101/src/components/user.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="user">
+    <h3>The user</h3>
+    <p>My username {{ username }}</p>
+  </div><!-- wrappiong div that's the root, only one allowed. -->
+
+</template>
+
+
+<script>
+
+export default {
+    data() {
+        return {
+          username: 'Jerome'
+      }
+    }
+}
+</script>
+
+<style>
+
+p{
+  font-style: cursive;
+}
+
+</style>
