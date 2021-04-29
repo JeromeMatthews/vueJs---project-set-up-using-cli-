04373a8dc050c318cc986293599d0d3d860ed490
Jerome Matthews <jerome_matthews@live.com>
2021-04-28 21:26:37 -0500

    Added routing functionalities

--- vue-framework-101/src/App.vue
+++ vue-framework-101/src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-      <app-user></app-user>
+    <router-view> </router-view>
+    <!-- where all our views will be outputred.
+    The router functionality comes here.-->
+
+
+      <!-- <app-user></app-user> -->
+
+  <!-- If we're creating links for differnt views in the app
+  They are done with the router-link tag -->
+
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/blog/">Blog</router-link>
+    <router-link to="account">Account</router-link>
+
+
       <hr>
       <p>This content is outside of the child components (in the root app component), <br>
       so the styling applied in them won't be seen here.</p>
--- vue-framework-101/src/main.js
+++ vue-framework-101/src/main.js
@@ -2,10 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import User from './components/User.vue'
 import Info from './components/Info.vue' // if we want to do it globally
+import VueRouter from 'vue-router'
+
+// SPA view routes:
+import Blog from './components/Blog.vue'
+import Account from './components/Account.vue'
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path:'/blog', component: Blog},
+  {path: '/account', component: Account},
+  {path: '/', component: User},
+]
+
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
+
 Vue.component('app-user', User);
 
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
